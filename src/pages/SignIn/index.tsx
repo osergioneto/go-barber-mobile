3d73c8beb0d39a672b4f50e7ac827b9104eb74fc
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-08-02 22:57:49 -0300

    feat: add button component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -9,6 +12,11 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input />
+      <Input />
+
+      <Button>Entrar</Button>
     </Container>
   );
 };
